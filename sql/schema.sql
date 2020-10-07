4871f879c89bea59847eb4857c86c858e1535916
Ludo <Kiluhasenpai@gmail.com>
2020-10-07 14:18:24 +0200

    avec l'insert

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,3 +3,9 @@ CREATE TABLE position (
   	longitude REAL NOT NULL,
   	latitude REAL NOT NULL
 );
+
+INSERT INTO position (
+ longitude, latitude) VALUES 
+ (1.2167, 44.3333),
+ (1.48494, 39.0233);
+
